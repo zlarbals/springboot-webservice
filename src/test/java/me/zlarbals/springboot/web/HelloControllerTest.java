b5bd26c8acf40e3e47ede3df2f6933ad8f1da468
zlarbals <zlarbals@gmail.com>
2021-02-02 15:16:52 +0900

    naver login

--- src/test/java/me/zlarbals/springboot/web/HelloControllerTest.java
+++ src/test/java/me/zlarbals/springboot/web/HelloControllerTest.java
@@ -1,9 +1,14 @@
 package me.zlarbals.springboot.web;
 
+import me.zlarbals.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +19,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE,classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -31,6 +40,7 @@ public class HelloControllerTest {
 
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
